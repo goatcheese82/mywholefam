595a4be8ce8fb85735591cfcb8786897d458e15e
goatcheese <euphoriawebdev@gmail.com>
2019-12-19 15:31:14 -0600

    added user crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :first_name,
+            :last_name,
+            :email,
+            :phone_number,
+            :birth_date,
+            :location,
+            :birthplace
+        )
+    end
 end
